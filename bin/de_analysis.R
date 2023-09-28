7dc2b5170555468f363c9afce21c0afc67708d77
Philip R. Davies <philip.r.davies@phe.gov.uk@phe-pf0ypusb.eud.unix.phe.gov.uk>
2023-09-28 15:05:28 +0100

    add plots in the pdf for verifying directionality of LFC

--- bin/de_analysis.R
+++ bin/de_analysis.R
@@ -87,6 +87,16 @@ edger_res <- topTags(qlf, n=nrow(y), sort.by="PValue")[[1]]
 pdf("de_analysis/results_dge.pdf")
 plotMD(qlf)
 abline(h=c(-1,1), col="blue")
+# circle and label the most signifficant gene for direction verification
+for(j in 1:10)
+  points(edger_res$logCPM[1], edger_res$logFC[1],
+         col = 3)
+text(edger_res$logCPM[1], edger_res$logFC[1],
+     labels = rownames(edger_res)[1],col="black",cex=1,pos=3)
+boxplot(counts ~ condition, 
+        data = data.frame(counts = y$counts[rownames(edger_res)[1],],
+                          condition = coldata$condition),
+        main = rownames(edger_res)[1])
 plotQLDisp(fit)
 
 write.table(as.data.frame(edger_res), file="de_analysis/results_dge.tsv", sep="\t")
