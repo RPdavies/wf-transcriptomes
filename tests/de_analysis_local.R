065b90c475b66cc616c9a4481ff15bd62693f01a
Philip R. Davies <philip.r.davies@phe.gov.uk@phe-pf0ypusb.eud.unix.phe.gov.uk>
2023-09-29 16:16:35 +0100

    added error to check for presence of control condition

--- tests/de_analysis_local.R
+++ tests/de_analysis_local.R
@@ -1,13 +1,21 @@
 coldata <- read.csv("../../nextflow/sample_sheet_testPaired.csv",
-                    stringsAsFactors = T)
+                    row.names="alias", sep=",", stringsAsFactors=TRUE)
+coldata$sample_id <- rownames(coldata)
+#coldata$condition <- factor(coldata$condition, levels=rev(levels(coldata$condition)))
+
+# check if control condition exists, sets as reference 
+if(!"control" %in% coldata$condition)
+  stop("sample_sheet.csv does not contain 'control' 
+       condition - unable to set reference")
 coldata$condition <- relevel(coldata$condition, ref = "control")
 
 gene_cts <- read.delim("../../nextflow/de_analysis/all_gene_counts.tsv")
 
-# Not including an intercept leads to broken results! 
+# Not including an intercept leads to broken results! Likely because 
+# explicit contrasts matrix is then expected
+
 #design <- model.matrix(~ 0 + ID + condition, data = coldata)
 #design <- model.matrix(~ 0 + condition, data = coldata)
-
 #design <- model.matrix(~ ID + condition, data = coldata)
 design <- model.matrix(~ condition, data = coldata)
 
@@ -21,12 +29,25 @@ head(fit$coefficients)
 qlf <- glmQLFTest(fit)
 edger_res <- topTags(qlf, n=nrow(y), sort.by="PValue")[[1]]
 
-head(edger_res)
-
-length(which(edger_res$PValue < 0.05 & edger_res$logFC > 0))
-length(which(edger_res$PValue < 0.05 & edger_res$logFC < 0))
+write.table(as.data.frame(edger_res), file="results_dge_correct.tsv", sep="\t")
 
 plotMD(qlf)
 abline(h=c(-1,1), col="blue")
 plotQLDisp(fit)
 plotBCV(y)
+
+i <- 1
+png("test.png", width = 800)
+par(mfrow = c(1,2))
+boxplot(counts ~ condition, 
+        data = data.frame(counts = y$counts[rownames(edger_res)[i],],
+                          condition = coldata$condition),
+        main = rownames(edger_res)[i])
+
+plotMD(qlf)
+for(j in 1:10)
+points(edger_res$logCPM[i], edger_res$logFC[i],
+       col = 3)
+text(edger_res$logCPM[i], edger_res$logFC[i],
+     labels = rownames(edger_res)[i],col="black",cex=1,pos=3)
+dev.off()
